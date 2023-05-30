b0a33a0981390fb9deea8700dd04ffcfee59b723
edgarromero1994 <edgarromerocuc94@gmail.com>
2023-05-30 09:27:12 -0600

    Nuevos Cambios en commit

--- src/components/Piepagina.jsx
+++ src/components/Piepagina.jsx
@@ -5,18 +5,16 @@ const Piepagina = () => {
   return (
     <div className='piepaginacontent'>
       <div className='redes-sociales'>
-        <a href='#' className='redes-sociales-icono'>
+       
           <FaFacebook />
-        </a>
-        <a href='#' className='redes-sociales-icono'>
+ 
           <FaInstagram />
-        </a>
-        <a href='#' className='redes-sociales-icono'>
+     
           <FaYoutube />
-        </a>
-        <a href='#' className='redes-sociales-icono'>
+      
+  
           <FaWhatsapp />
-        </a>
+   
       </div>
       <div>
         <p className='kursiva'>Guatemala, C.A año 2023 </p>
